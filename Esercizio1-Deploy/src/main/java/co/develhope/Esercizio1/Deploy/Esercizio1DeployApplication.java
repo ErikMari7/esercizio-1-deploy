c7d5bab245eeea0d57deb4d60975e31782fe2e3c
Erik <mariglianoerik@gmail.com>
2024-03-07 16:04:23 +0100

    esercizio finito

---
+++ Esercizio1-Deploy/src/main/java/co/develhope/Esercizio1/Deploy/Esercizio1DeployApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.Esercizio1.Deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio1DeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio1DeployApplication.class, args);
+	}
+
+}
---
+++ Esercizio1-Deploy/src/test/java/co/develhope/Esercizio1/Deploy/Esercizio1DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.Esercizio1.Deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio1DeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
